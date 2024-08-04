7ae90052234a455d2675dd188d99c3c7612ba741
Sujal Kodamadanchirayil <sujal23.ks@gmail.com>
2024-08-04 19:59:23 +1000

    basic website setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, Divya!</h1>
+        <p>Welcome to my first React app!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
